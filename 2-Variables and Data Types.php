fc0103363233bb5bdc1996e0fed520b26c6636b6
Araz Omarov <05arazomarov@gmail.com>
2025-09-25 16:45:36 +0400

    2-Variables and Data Types.php

---
+++ 2-Variables and Data Types.php
@@ -0,0 +1,10 @@
+<?php
+$name = "Araz";                         # string ->  hərflər, sözlər və cümlələr 
+$age = 14;                              # integer -> ədədlər və rəqəmlər
+$height = 1.71;                         # float -> ondalıklı və ya vergüllü rəqəmlər
+$student = true;                        # boolean -> true & false
+
+echo "Salam, mən $name.<br>";           # Salam, mən Araz.
+echo "Mənim $age yaşım var.<br>";       # Mənim 14 yaşım var.
+echo "Mənim boyum $height-dir.<br>";    # Mənim boyum 1.71-dir.
+?> 
